58d6c6723eca9d98661b524864be7dcb702096a3
Ali Haidar <alibin.khalid@devsinc.com>
2023-08-08 19:09:16 +0500

    utility_methods.rb modified for third and fourth part

--- source/utility_methods.rb
+++ source/utility_methods.rb
@@ -18,3 +18,12 @@ def loop_running (file_path,array,flag)
     end
     return flag
 end
+
+def print_low_temp (i,count_for_day)
+    min_temp_count = i[@min_temp_index].to_i
+    check_for_temp_?(min_temp_count,count_for_day)
+    min_temp_count.times do
+        print "+".colorize(:blue)
+    end
+    return min_temp_count
+end
